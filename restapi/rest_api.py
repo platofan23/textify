fbe1576bd4c5bc7c6e3b133697b7c5df88b1e864
Hein0002 <79054346+Hein0002@users.noreply.github.com>
2024-11-19 18:23:08 +0100

    Files are now uploaded to MongoDB

--- restapi/rest_api.py
+++ restapi/rest_api.py
@@ -1,26 +1,40 @@
-import os
-import shutil
+
 import configparser
 from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import gridfs
 
 # Load configuration from config.ini
 config = configparser.ConfigParser()
 config.read('../config.ini')
 
-
+# Set up Flask
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = config['REST']['UPLOAD_FOLDER']
 app.config['MAX_CONTENT_LENGTH'] = int(config['REST']['MAX_CONTENT_LENGTH_MB']) * 1024 * 1024 # Set max file size to xMB
 MAX_TOTAL_SIZE = int(config['REST']['MAX_TOTAL_SIZE_GB']) * 1024 * 1024 * 1024 # Set max total file size to xGB
 ALLOWED_EXTENSIONS = set(config['REST']['ALLOWED_EXTENSIONS'].replace(" ", "").split(','))
 
+# MongoDB connection
+client = MongoClient(config['MONGO_DB']['MONGO_URI'])
+db = client[config['MONGO_DB']['MONGO_UPLOADS']]
+fs = gridfs.GridFS(db, collection=config['MONGO_DB']['MONGO_COLLECTION'])
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+'''
+Route to upload files
+POST request should contain:
+- 'files' part with the files to be uploaded
+- 'User' part with the user name
+- 'Title' part with the title of the files
+'''
 @app.route('/upload_files', methods=['POST'])
 def upload_files():
+
     if 'files' not in request.files:
         return 'No file part'
     if 'User' not in request.headers:
@@ -31,23 +45,30 @@ def upload_files():
     files = request.files.getlist('files')
     total_size = 0
 
-    user_folder_path = os.path.join(app.config['UPLOAD_FOLDER'], request.headers.get("User"))
-    os.makedirs(user_folder_path, exist_ok=True)
-    title_folder_path = os.path.join(user_folder_path, request.headers.get("Title"))
-    os.makedirs(title_folder_path, exist_ok=True)
+    file_ids = []
 
     for file in files:
+
         if file.filename == '':
             return 'No selected file'
-        if file and allowed_file(file.filename):
+
+        if allowed_file(file.filename):
             total_size += len(file.read())
             file.seek(0)
+
+            # Check if total file size exceeds limit
             if total_size > MAX_TOTAL_SIZE:
-                shutil.rmtree(title_folder_path)
-                return 'Total file size exceeds 10GB'
-            file.save(os.path.join(title_folder_path, file.filename))
+                # Delete files already uploaded
+                for file_id in file_ids:
+                    fs.delete(file_id)
+                return 'Total file size exceeds ' + str(MAX_TOTAL_SIZE) + ' bytes'
+
+            file_ids.append(fs.put(file, filename=file.filename, user=request.headers.get("User")
+                                   , title=request.headers.get("Title")))
+
         else:
-            return 'Invalid file type or file too large'
+            return 'Invalid file type'
+
     return 'Files uploaded successfully'
 
 if __name__ == '__main__':
