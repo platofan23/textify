3e1c6283f74bd4617159f03cf282f7503adf4856
Joel <58483712+platofan23@users.noreply.github.com>
2025-01-11 21:27:29 +0100

    Update main.py for running in docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from flask_restful import Api
+
 from backend.app.start import register_routes, preload_models, run_tests, create_app
 
 # Initialize Flask app
@@ -8,11 +9,14 @@ app, config_manager, cache_manager = create_app()
 api = Api(app)
 register_routes(api, config_manager, cache_manager)
 
+run_tests()
+preload_models(config_manager, cache_manager)
+
 
 # Start the Flask application
 if __name__ == '__main__':
-    run_tests()  # 1. Run unit tests
-    #preload_models(config_manager, cache_manager)  # 2. Load models
+    run_tests()
+    preload_models(config_manager, cache_manager)
     port = config_manager.get_config_value('REST', 'PORT', int, default=5555)
     host = config_manager.get_config_value('REST', 'HOST', str, default='127.0.0.1')
     app.run(host=host, port=port, debug=True)
