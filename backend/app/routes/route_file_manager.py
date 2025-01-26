a8822b9e79f6260448d241aeaef71b7e01af32a9
Joel <58483712+platofan23@users.noreply.github.com>
2025-01-26 21:03:58 +0100

    change how to give config

--- backend/app/routes/route_file_manager.py
+++ backend/app/routes/route_file_manager.py
@@ -3,19 +3,28 @@ import configparser
 from flask import send_file
 from flask_restful import Resource, reqparse
 from werkzeug.datastructures import FileStorage
-from backend.app.utils import Logger, MongoDBManager
+from backend.app.utils import MongoDBManager, ConfigManager
+from backend.app.utils.util_logger import Logger
 
 # Load configuration
-config = configparser.ConfigParser()
-config.read('./config/config.ini')
+CONFIG_PATH = os.getenv("CONFIG_PATH", "./config/config.ini")  # Standardmäßiger Pfad zur Konfigurationsdatei
+DOCKER_CONFIG_PATH = "./config/docker.ini"
+
+config_path = CONFIG_PATH
 if os.getenv("IsDocker"):
-    config.read('./config/docker.ini')
+    config_path = DOCKER_CONFIG_PATH
+
+if not os.path.exists(config_path):
+    raise FileNotFoundError(f"Configuration file not found at {config_path}")
+
+config_manager = ConfigManager(config_path)
+config = configparser.ConfigParser()
+config.read(config_path)
 
 MAX_TOTAL_SIZE = int(config['REST']['MAX_TOTAL_SIZE_GB']) * 1024 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(config['REST']['ALLOWED_EXTENSIONS'].replace(" ", "").split(','))
 
 # MongoDB setup
-config_manager = ConfigManager(config)
 mongo_manager = MongoDBManager(config_manager)
 
 # Helper function to check allowed file types
@@ -25,48 +34,61 @@ def allowed_file(filename):
 # Resource for file upload
 class UploadFile(Resource):
     def post(self):
-        # Validate parameters
         parser = reqparse.RequestParser()
         parser.add_argument('User', location='headers', required=True, help="User header is required")
         parser.add_argument('Title', location='headers', required=True, help="Title header is required")
-        parser.add_argument('files', type=FileStorage, location='headers', action='append', required=True)
+        parser.add_argument('files', type=FileStorage, location='files', action='append', required=True)
         args = parser.parse_args()
 
         files = args['files']
         user = args['User']
         title = args['Title']
 
+        if not files or len(files) == 0:
+            Logger.error('No files uploaded')
+            return {'error': 'No files uploaded'}, 400
+
         total_size = 0
         file_ids = []
 
-        # Save files to MongoDB
-        for file in files:
-            if file.filename == '':
-                Logger.error('No selected file')
-                return {'error': 'No selected file'}, 400
-            if allowed_file(file.filename):
-                total_size += len(file.read())
-                file.seek(0)
+        try:
+            for file in files:
+                if file.filename == '':
+                    Logger.error('No selected file')
+                    return {'error': 'No selected file'}, 400
+
+                if not allowed_file(file.filename):
+                    Logger.error('Invalid file type')
+                    return {'error': f"Invalid file type: {file.filename}"}, 415
+
+                file_size = len(file.read())
+                file.seek(0)  # Setze den Lesezeiger zurück
+                total_size += file_size
+
                 if total_size > MAX_TOTAL_SIZE:
-                    # Reverse changes by deleting uploaded files
                     for file_id in file_ids:
                         mongo_manager.delete_documents(config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'], {'_id': file_id}, use_GridFS=True)
                     Logger.error(f'Total file size exceeds {MAX_TOTAL_SIZE} bytes')
                     return {'error': f'Total file size exceeds {MAX_TOTAL_SIZE} bytes'}, 413
-                file_id = mongo_manager.insert_document(config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'], {'file': file, 'filename': file.filename, 'user': user, 'title': title}, use_GridFS=True)
+
+                file_id = mongo_manager.insert_document(
+                    config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'],
+                    {'file': file, 'filename': file.filename, 'user': user, 'title': title},
+                    use_GridFS=True
+                )
                 file_ids.append(file_id)
                 Logger.info(f'File {file.filename} uploaded successfully')
-            else:
-                Logger.error('Invalid file type')
-                return {'error': 'Invalid file type'}, 415
 
-        Logger.info('Files uploaded successfully')
-        return {'message': 'Files uploaded successfully'}, 201
+            Logger.info('Files uploaded successfully')
+            return {'message': 'Files uploaded successfully'}, 201
+
+        except Exception as e:
+            Logger.error(f"Error during upload: {str(e)}")
+            return {'error': f"Error during upload: {str(e)}"}, 500
 
 # Resource for file download
 class DownloadFile(Resource):
     def get(self):
-        # Validate parameters
         parser = reqparse.RequestParser()
         parser.add_argument('filename', location='headers', type=str, required=True, help="Filename is required")
         parser.add_argument('user', location='headers', type=str, required=True, help="User is required")
@@ -78,16 +100,19 @@ class DownloadFile(Resource):
         title = args['title']
 
         try:
-            # Retrieve file from MongoDB
-            files = mongo_manager.find_documents(config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'], {'filename': filename, 'user': user, 'title': title}, use_GridFS=True)
+            files = mongo_manager.find_documents(
+                config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'],
+                {'filename': filename, 'user': user, 'title': title},
+                use_GridFS=True
+            )
             if not files:
                 Logger.error('File not found')
                 return {'error': 'File not found'}, 404
 
             file = files[0]
             Logger.info(f'File {filename} retrieved successfully')
-            # Send file
             return send_file(file, download_name=filename, as_attachment=True)
+
         except Exception as e:
             Logger.error(f'Error occurred: {str(e)}')
             return {'error': f'Error occurred: {str(e)}'}, 500
@@ -95,7 +120,6 @@ class DownloadFile(Resource):
 # Resource for file deletion
 class DeleteFile(Resource):
     def delete(self):
-        # Validate parameters
         parser = reqparse.RequestParser()
         parser.add_argument('filename', location='headers', type=str, required=True, help="Filename is required")
         parser.add_argument('user', location='headers', type=str, required=True, help="User is required")
@@ -107,14 +131,18 @@ class DeleteFile(Resource):
         title = args['title']
 
         try:
-            # Delete file from MongoDB
-            result = mongo_manager.delete_documents(config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'], {'filename': filename, 'user': user, 'title': title}, use_GridFS=True)
+            result = mongo_manager.delete_documents(
+                config['MONGO_DB']['MONGO_USER_FILES_COLLECTION'],
+                {'filename': filename, 'user': user, 'title': title},
+                use_GridFS=True
+            )
             if result.deleted_count == 0:
                 Logger.error('File not found')
                 return {'error': 'File not found'}, 404
 
             Logger.info(f'File {filename} deleted successfully')
             return {'message': 'File deleted successfully'}, 200
+
         except Exception as e:
             Logger.error(f'Error occurred: {str(e)}')
             return {'error': f'Error occurred: {str(e)}'}, 500
