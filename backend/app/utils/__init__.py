d4a3d73995fc6ca679050758ff950a8e4f4f4ed0
Joel <58483712+platofan23@users.noreply.github.com>
2025-02-02 15:59:02 +0100

    Rename util_text_manager

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,7 +1,7 @@
 from .util_cache_mananger import CacheManager
 from .util_config_manager import ConfigManager, TranslationModel
 from .util_pdf_processor import PDFProcessor
-from .utils_text_manager import preprocess_text, split_text_into_chunks, join_and_split_translations
+from .util_text_manager import preprocess_text, split_text_into_chunks, join_and_split_translations
 from .util_mongo_manager import MongoDBManager
 from .util_logger import Logger
 
